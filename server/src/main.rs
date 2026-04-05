48e8ee2c35b862174e9b27862871cd9294e2bc39
Keith Dao <keith.dao.248@gmail.com>
2026-04-05 17:40:38 +1000

    feat(server): add entry point for http server.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//! Entrypoint for server.
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = axum::Router::new().route("/", axum::routing::get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
